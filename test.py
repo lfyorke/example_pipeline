0e51da4caa7a17bc38e1202a0a0431a3ae49dd17
lfyorke <Leo.Yorke@keyrus.com>
2018-08-23 17:22:01 +0200

    Added functionality to flatten data into feature dictionary, remove lists and delete unwanted columns

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-
+import json
+import collections
 
 class TestPipeline:
     """
@@ -9,14 +10,70 @@ class TestPipeline:
         self.source_file_path = source_file_path
 
 
-    def extract(self):
+    def flatten(self, d, parent_key='', sep='_'):
+        """
+
+        :param d:
+        :param parent_key:
+        :param sep:
+        :return:
+        """
+        items = []
+        for k, v in d.items():
+            new_key = parent_key + sep + k if parent_key else k
+            if isinstance(v, collections.MutableMapping):
+                items.extend(self.flatten(v, new_key, sep=sep).items())
+            else:
+                items.append((new_key, v))
+        return dict(items)
+
+    def clean_imp(self, data):
+        """
+
+        :param data:
+        :return:
+        """
+        if 'imp' in data:
+            data['imp'] = data['imp'][0]
+        return data
+
+
+    def del_features(self, data, features=['imp_banner_battr']):
+        """
+
+        :param data:
+        :param features:
+        :return:
+        """
+        for feat in features:
+            if feat in data:
+                del data[feat]
+        return data
+
+
+    def remove_lists(self, data):
+        """
+
+        :param data:
+        :return:
+        """
+
+        for key, value in data.items():
+            if isinstance(value, list):
+                data[key] = value[0]
+        return data
+
+    def extract(self, source_file_path):
         """
         Method to read dataset from input filepath.
 
         :param file_path:
         :return:
         """
-        pass
+        with open(source_file_path) as f:
+            string = f.readline().strip()
+            d = json.loads(string)
+            return d
 
     def transform(self):
         """
@@ -43,7 +100,12 @@ class TestPipeline:
         :return:
         """
 
-        self.extract()
+        test = self.extract(self.source_file_path)
+        flattened = self.flatten(test)
+        cleaned = self.remove_lists(self.flatten(self.clean_imp(flattened)))
+        feature_dict = self.del_features(cleaned)
+
+        print(feature_dict)
         self.load()
         self.transform()
 
